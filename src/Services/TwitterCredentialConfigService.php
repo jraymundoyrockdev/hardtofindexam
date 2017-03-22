0cd374a4b4b9d302aad6fa76fd48885af7e7d7f8
Jem Raymundo <jraymundo.yrockdev@gmail.com>
2017-03-23 04:15:47 +0800

    Removed my personal twitter auth credentials

--- src/Services/TwitterCredentialConfigService.php
+++ src/Services/TwitterCredentialConfigService.php
@@ -13,10 +13,10 @@ class TwitterCredentialConfigService
     {
         $credentials = [
             'default' => [
-              'oauth_access_token' => "724428917134630913-dwcK73XpPIwi6hmS5kVC6ID1HCSWYKf",
-              'oauth_access_token_secret' => "TWI5XG0JmVB8zyf3hBKb1lYmuW74pwn1KeBmolqmo2Q8Z",
-              'consumer_key' => "yzgCtLGl3dDWykRwj7TvX8Ib1",
-              'consumer_secret' => "1nciVYpCDB2hKynr89oUeteQRNQCdZQWARIeCRQHgKgvwwxnYP"
+              'oauth_access_token' => "",
+              'oauth_access_token_secret' => "",
+              'consumer_key' => "",
+              'consumer_secret' => ""
             ]
         ];
 
